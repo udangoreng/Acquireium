3172dc06634157080177766eef9f3e7cb8349dda
udangoreng <divanandasasikirana@gmail.com>
2025-10-23 09:26:35 +0700

    Static Data

--- index.js
+++ index.js
@@ -1,12 +1,165 @@
+var products = {
+    equipment: {
+        product1: {
+            name: "ISTA Hang-On Filter Inlet Sponge",
+            price: 150000,
+            stock: 10,
+            description: "Hang on filter inlet sponge",
+            imgPath: "img/filter.jpg"
+        },
+        product2: {
+            name: "UNS Mighty HOB Aquarium Filter",
+            price: 200000,
+            stock: 5,
+            description: "The Mighty HOB Aquarium Filters (HOB = Hang On Back Filter) are compact filters for nano aquariums. The Hang On filters are available in three different sizes and provide the necessary filtration in freshwater planted nano aquariums. These units are easy to maintain, feature adjustable flow and levelling knobs and are made from durable materials for optimum performance and longevity. Each filter is pre-loaded with media, is self-priming and can be quickly installed at the edge of any nano tank.",
+            imgPath: "img/equip.JPG"
+        },
+        product3: {
+            name: "EHEIM InstallationsKit 2 Pressure Side 17 mm",
+            price: 400000,
+            stock: 5,
+            description: "The EHEIM Basic Installation Set 2 is used to connect to the pressure side of all external filters. Thus, it can be determined individually, where the water in the aquarium should flow. The set has a safe and watertight hose connection.\n The installation kit allows hose cleaning without removing the hose connection. In addition, it ensures a secure and permanent attachment in the basin through the supplied suction cups.",
+            imgPath: "img/filter-2.jpg"
+        }
+    },
+    plants: {
+        product1: {
+            name: "Anubias Plant",
+            price: 50000,
+            species:"Anubias Nana Pinto",
+            stock: 20,
+            description: "Low maintenance aquatic plant",
+            imgPath: "img/anubias.png"
+        },
+        product2: {
+            name: "Java Moss",
+            price: 30000,
+            species:"Vesicularia dubyana",
+            stock: 25,
+            description: "The species is monoecious and perennial, with an irregularly branched stem. In the wild, it is found on soils, rocks, and trunks of trees.",
+            imgPath: "img/javafern.png"
+        },
+        product3: {
+            name: "Singapore Blood Red",
+            price: 25000,
+            species:"Rotala rotundifolia",
+            stock: 18,
+            description: "'Singapore Blood Red', also 'Blood Red Singapore', certainly has the most intense red colouring of all Rotala rotundifolia forms. Under the same conditions, this Rotala has a darker red shade than R. rotundifolia 'Colorata'. As with normal Rotala rotundifolia, the shoots grow mostly upright to ascending, in very strong light also more or less horizontally. As with 'normal' Rotala rotundifolia, the shoots grow to over 50 cm long, but look better if they are kept about 20 - 40 cm high by cutting and replanting cuttings. They branch quite well, the bushy growth is promoted by occasional pruning.",
+            imgPath: "img/rotala.jpg"
+        },
+        product4: {
+            name: "Grass leaved bladderwort",
+            price: 30000,
+            species:"Utricularia graminifolia",
+            stock: 7,
+            description: "Utricularia graminifolia is a grass-like bladderwort species from the Asian tropics that grows into a lush bright light green lawn. It is thus quite popular in aquascaping, too. It is not always easy to cultivate, but under favourable conditions (lots of light, soft water, CO2, macro- and micronutrient fertilisation) it is a fast grower.",
+            imgPath: "img/grass.jpg"
+        },
+    },
+    fish: {
+        product1: {
+            name: "Male Tangerine Koi Betta",
+            price: 25000,
+            species:"Betta splendens",
+            stock: 30,
+            description: "The Male Tangerine Koi is a majestic variant admired for its marbled orange, white and sometimes black coloration. Though delicate-looking, they are hardy and intelligent, thriving with proper care in a well-maintained aquarium. A varied diet of high-quality Betta pellets, frozen or live foods such as Bloodworms, Brine shrimp, and Daphnia is ideal for their continued health. A gentle filter is recommended and be sure to prevent strong currents for this Betta. With proper care, the Male Tangerine Koi Betta can live 2-4 years, serving as a stunning and mesmerizing aquatic pet.",
+            imgPath: "img/betafish.jpg"
+        },
+        product2: {
+            name: "Fancy Female Guppy, Pack of 6",
+            price: 15000,
+            species:"Poecilia reticulata",
+            stock: 50,
+            description: "Assorted Fancy Female Guppies are a beautiful, easy to care for, vibrant and hardy option for the beginner hobbyist. The males are smaller in size and tend to be more colorful than the feamles. It is not recommended to add multiple males to the aquarium because they can become territorial. They are peaceful fish that will do great in community tanks. Be aware that these are livebearers so when adding females and males that you can end up with more than when you started. This Guppy will eat just about anything offered from frozen freshwater foods to pellets, freeze dried and flakes.",
+            imgPath: "img/guppies.jpg"
+        },
+        product3: {
+            name: "Fancy Female Guppy, Pack of 6",
+            price: 15000,
+            species:"Poecilia reticulata",
+            stock: 50,
+            description: "Assorted Fancy Female Guppies are a beautiful, easy to care for, vibrant and hardy option for the beginner hobbyist. The males are smaller in size and tend to be more colorful than the feamles. It is not recommended to add multiple males to the aquarium because they can become territorial. They are peaceful fish that will do great in community tanks. Be aware that these are livebearers so when adding females and males that you can end up with more than when you started. This Guppy will eat just about anything offered from frozen freshwater foods to pellets, freeze dried and flakes.",
+            imgPath: "img/guppies.jpg"
+        },
+        product4: {
+            name: "Fancy Female Guppy, Pack of 6",
+            price: 15000,
+            species:"Poecilia reticulata",
+            stock: 50,
+            description: "Assorted Fancy Female Guppies are a beautiful, easy to care for, vibrant and hardy option for the beginner hobbyist. The males are smaller in size and tend to be more colorful than the feamles. It is not recommended to add multiple males to the aquarium because they can become territorial. They are peaceful fish that will do great in community tanks. Be aware that these are livebearers so when adding females and males that you can end up with more than when you started. This Guppy will eat just about anything offered from frozen freshwater foods to pellets, freeze dried and flakes.",
+            imgPath: "img/guppies.jpg"
+        },
+        product5: {
+            name: "Fancy Female Guppy, Pack of 6",
+            price: 15000,
+            species:"Poecilia reticulata",
+            stock: 50,
+            description: "Assorted Fancy Female Guppies are a beautiful, easy to care for, vibrant and hardy option for the beginner hobbyist. The males are smaller in size and tend to be more colorful than the feamles. It is not recommended to add multiple males to the aquarium because they can become territorial. They are peaceful fish that will do great in community tanks. Be aware that these are livebearers so when adding females and males that you can end up with more than when you started. This Guppy will eat just about anything offered from frozen freshwater foods to pellets, freeze dried and flakes.",
+            imgPath: "img/guppies.jpg"
+        },
+        product6: {
+            name: "Fancy Female Guppy, Pack of 6",
+            price: 15000,
+            species:"Poecilia reticulata",
+            stock: 50,
+            description: "Assorted Fancy Female Guppies are a beautiful, easy to care for, vibrant and hardy option for the beginner hobbyist. The males are smaller in size and tend to be more colorful than the feamles. It is not recommended to add multiple males to the aquarium because they can become territorial. They are peaceful fish that will do great in community tanks. Be aware that these are livebearers so when adding females and males that you can end up with more than when you started. This Guppy will eat just about anything offered from frozen freshwater foods to pellets, freeze dried and flakes.",
+            imgPath: "img/guppies.jpg"
+        }
+    }
+};
+
 var selectedId = 0;
 var amount = 0;
-var stock = 7;
+var stock;
+var category;
+var currentPage = location.pathname.substring(5);
 const amountIn = document.getElementById("amount");
 const imgModal = document.getElementById("modalImage");
 const imgModal2 = document.getElementById("pImage");
 
+function getCategory() {
+    switch (currentPage) {
+        case 'shopequip.html':
+            category = 'equipment';
+            break;
+
+        case 'shopplant.html':
+            category = 'plants';
+            break;
+
+        case 'shopfish.html':
+            category = 'fish';
+            break;
+
+        default:
+            break;
+    }
+}
+
+function getAllProducts() {
+    getCategory();
+    const currentCategory = products[category];
+
+    for (let productId in currentCategory) {
+        const product = currentCategory[productId];
+
+        const cardId = document.getElementById(productId);
+        const name = cardId.getElementsByTagName("h6")[0];
+        const spec = cardId.getElementsByTagName("p")[0];
+        const price = cardId.querySelector("#price");
+        const img = cardId.parentElement.getElementsByTagName("img")[0];
+        
+        name.textContent = product.name;
+        spec.textContent = product.species;
+        price.textContent = product.price;
+        img.src = product.imgPath;
+    }
+
+}
+
 function add() {
-    if (amount < stock) {
+    const currentStock = parseInt(document.getElementById("mStock").textContent);
+    if (amount < currentStock) {
         amount += 1;
         amountIn.value = amount;
         console.log(amount);
@@ -29,16 +182,20 @@ function SeeDetails(Id) {
     const spec = product.getElementsByTagName("p")[0];
     const price = document.getElementById("price");
     const img = product.parentElement.getElementsByTagName("img")[0];
+    const desc =  products[category][Id].description;
+    const stock =  products[category][Id].stock;
 
     const mSpecies = document.getElementById("mSpec");
     const mStock = document.getElementById("mStock");
     const mPrice = document.getElementById("mPrice");
     const mName = document.getElementById("mName");
+    const mDesc = document.getElementById("mDesc");
 
     mName.textContent = name.textContent;
-    mSpecies.textContent = spec.textContent;
+    mSpecies.textContent = spec.textContent ? spec.textContent : '-';
     mPrice.textContent = price.textContent;
     mStock.textContent = stock;
+    mDesc.textContent = desc;
 
     if (img) {
         imgModal.src = img.src;
@@ -56,7 +213,7 @@ function proceed() {
     if (amount > stock) {
         alert('Not Enough Stock! Please Wait For Us To Restock!')
     }
-    
+
     const product = document.getElementById(selectedId);
     const name = product.getElementsByTagName("h6")[0];
     const spec = product.getElementsByTagName("p")[0];
@@ -71,7 +228,7 @@ function proceed() {
     const subtotal = document.getElementById("pSubTotal");
 
     mName.textContent = name.textContent;
-    mSpecies.textContent = spec.textContent;
+    mSpecies.textContent = spec.textContent ? spec.textContent : '-';
     mPrice.textContent = "Rp." + (price.textContent);
     subtotal.textContent = "Rp." + (price.textContent * amount);
     total.textContent = "Rp." + (parseInt(price.textContent * amount) + 25000);
@@ -103,11 +260,13 @@ function success() {
         return;
     }
 
-    stock -= amount;
+    getCategory();
+
+    products[category][selectedId].stock -= amount;
     amount = 0;
     amountIn.value = 0;
 
-    if (stock == 0) {
+    if (products[category][selectedId].stock == 0) {
         stockShow.textContent = 'Out Of Stock!';
     }
 
